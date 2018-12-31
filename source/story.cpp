9e8653b6224042706f104adcf3fd625d3aeeca96
chussong <charles.hussong@gmail.com>
2018-12-31 21:41:10 +0900

    Add full comment parsing, completing 1.1

--- source/story.cpp
+++ source/story.cpp
@@ -1,6 +1,7 @@
 #include "story.hpp"
 
 #include <fstream>
+#include <iterator>
 
 namespace Spink {
 
@@ -15,12 +16,40 @@ const std::vector<Line>& Story::lines() const noexcept {
 Story Story::parse(const std::filesystem::path& inputPath) {
     Story output;
     output._name = inputPath.filename();
-
     std::ifstream inStream(inputPath);
-    for (std::string rawLine; inStream; std::getline(inStream, rawLine)) {
-        Line newLine = Line::parse(rawLine);
-        if (!newLine.empty()) output._lines.push_back(std::move(newLine));
+    std::string rawFile(std::istreambuf_iterator<char>{inStream}, {});
+
+    // erase block comments
+    std::string::size_type blockStart = rawFile.find("/*");
+    while (blockStart != std::string::npos) {
+        std::string::size_type blockEnd = rawFile.find("*/", blockStart+2);
+        if (blockEnd != std::string::npos) {
+            rawFile.erase(blockStart, blockEnd-blockStart+2);
+        } else {
+            break;
+        }
+        blockStart = rawFile.find("/*");
     }
+
+    // erase inline comments
+    while (true) {
+        std::string::size_type commentStart = rawFile.find("//");
+        if (commentStart == std::string::npos) break;
+        std::string::size_type commentEnd = rawFile.find("\n", commentStart+2);
+        // careful not to delete the newline
+        rawFile.erase(commentStart, commentEnd-commentStart);
+    }
+
+    // parse lines from whatever's left
+    std::string::size_type lineEnd = -1;
+    do {
+        std::string::size_type lineStart = lineEnd+1;
+        lineEnd = std::min(rawFile.find("\n", lineStart), rawFile.size());
+        Line newLine = Line::parse({rawFile.data() + lineStart, 
+                                    lineEnd - lineStart});
+        if (!newLine.empty()) output._lines.push_back(std::move(newLine));
+    } while (lineEnd < rawFile.size());
+
     return output;
 }
 
